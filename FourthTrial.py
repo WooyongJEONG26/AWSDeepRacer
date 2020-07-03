412d15897bdd0e982641b8dca3c5777861104a4a
WooyongJeong <dndyd0206@gmail.com>
2020-07-03 04:29:41 -0400

    Fith commit DeepRacer: FourthTrial

---
+++ FourthTrial.py
@@ -0,0 +1,30 @@
+def reward_function(params):
+    '''
+
+    :param params:
+    :return: reward(result of behaviour)
+    '''
+
+    # var
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    # boolean var
+    is_left_of_center = params['is_left_of_center']
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    reward = 0
+
+    if distance_from_center <= marker_3:
+        if is_left_of_center:
+            reward += 0.5
+        reward += 1.0
+    elif distance_from_center <= marker_2:
+        reward += 0.5
+    elif distance_from_center <= marker_1:
+        reward += 0.1
+    else:
+        reward += 1e-3
+
+    return float(reward)
