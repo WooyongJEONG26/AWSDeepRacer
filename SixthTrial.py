070a18180012b5057d471e68bcd33c6ccf0f1cae
WooyongJeong <dndyd0206@gmail.com>
2020-07-14 10:23:00 -0400

    SixthTrial.py: staying away from center

--- SixthTrial.py
+++ SixthTrial.py
@@ -1,29 +1,44 @@
 # needed for calculation for future
-import math
+
+# Straight line
+def is_straight():
+    return 0
+
+
+# Being right
+def is_right(is_left_of_center):
+    result = 0
+    if is_left_of_center:
+        result -= 0.3
+    else:
+        result += 0.3
+    return result
+
 
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
+    """
+    :param params:  track_width (float)
+                    distance_from_center (float)
+                    is_left_of_center (boolean)
+
+    :return: reward (float)
+    """
 
     # Read input parameters
-    track_width = params['track_width']
+    # track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     is_left_of_center = params['is_left_of_center']
 
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    # reward var
+    reward = 0
 
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
+    # Setting range from the center of track
+    if 0.0 < distance_from_center <= 0.2:
+        reward += is_right(is_left_of_center)
+        reward += 0.3
+    elif 0.2 <= distance_from_center < 0.6:
+        reward += is_right(is_left_of_center)
+        reward += 0.4
     else:
         reward = 1e-3  # likely crashed/ close to off track
-
     return float(reward)
